058187dc1b0d6cdfa74a0df989ed5cd27bf1b8dc
MattiaR <rovitomattia91@gmail.com>
2021-09-06 21:23:51 +0200

    add comics/home.blade.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('homepage');
+
+
+
+Route::get('/comics', function () {
+    return view('comics');
+})->name('sezione-comics');
